e8ddaa9ffcb9203afe417a0e1a9967122b491856
Jiazheng_Xing <77264390+jiazheng-xing@users.noreply.github.com>
2022-09-30 14:01:10 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -8,11 +8,13 @@ import re
 import datetime
 import time
 import sys
-import ddddocr
 
+
+# import ddddocr
 
 class ClockIn(object):
     """Hit card class
+
     Attributes:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
@@ -25,16 +27,16 @@ class ClockIn(object):
     LOGIN_URL = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
     BASE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
     SAVE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
-    CAPTCHA_URL = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
+    #     CAPTCHA_URL = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
     HEADERS = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
     }
-    
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.sess = requests.Session()
-        self.ocr = ddddocr.DdddOcr()
+        # self.ocr = ddddocr.DdddOcr()
 
     def login(self):
         """Login to ZJU platform"""
@@ -72,9 +74,9 @@ class ClockIn(object):
     def get_captcha(self):
         """Get CAPTCHA code"""
         resp = self.sess.get(self.CAPTCHA_URL)
-        captcha = self.ocr.classification(resp.content)
-        print("验证码：", captcha)
-        return captcha
+        # captcha = self.ocr.classification(resp.content)
+        # print("验证码：", captcha)
+        # return captcha
 
     def get_info(self, html=None):
         """Get hitcard info, which is the old info with updated new time."""
@@ -111,13 +113,18 @@ class ClockIn(object):
         # form change
         new_info['jrdqtlqk[]'] = 0
         new_info['jrdqjcqk[]'] = 0
-        new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
-        new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
-        new_info['sfqrxxss'] = 1    # 是否确认信息属实
+        new_info['sfsqhzjkk'] = 1  # 是否申领杭州健康码
+        new_info['sqhzjkkys'] = 1  # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+        new_info['sfqrxxss'] = 1  # 是否确认信息属实
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
         # new_info['verifyCode'] = self.get_captcha()
+        # add in 2022.07.08
+        new_info['sfymqjczrj'] = 2
+        new_info['ismoved'] = 4  #
+        new_info['internship'] = 3
+        new_info['sfcxzysx'] = 2
 
         # 2021.08.05 Fix 2
         magics = re.findall(r'"([0-9a-f]{32})":\s*"([^\"]+)"', html)
@@ -187,9 +194,9 @@ def main(username, password):
             print('已为您打卡成功！')
         else:
             print(res['m'])
-            if res['m'].find("已经") != -1: # 已经填报过了 不报错
+            if res['m'].find("已经") != -1:  # 已经填报过了 不报错
                 pass
-            elif res['m'].find("验证码错误") != -1: # 验证码错误
+            elif res['m'].find("验证码错误") != -1:  # 验证码错误
                 print('再次尝试')
                 time.sleep(5)
                 main(username, password)
